4dfc2f1fe4aa5ded1795a9fd3d528bc9af7e248c
Artem Ostapenko <109479361+Krahjotdaan@users.noreply.github.com>
2024-05-23 10:49:02 +0300

    Update forms.py

--- generation/forms.py
+++ generation/forms.py
@@ -6,25 +6,39 @@ class StyleForm(forms.Form):
     style = forms.ChoiceField(
         label='Выберите стиль*',
         required=True,
-        choices=(('Гиперреализм', 'Гиперреализм'), 
-                 ('Аниме', 'Аниме')),
+        choices=(
+                    ('Гиперреализм', 'Гиперреализм'), 
+                    ('Аниме', 'Аниме'),
+                    ('Детская книжная иллюстрация', 'Детская книжная иллюстрация'),
+                    ('Фэнтези', 'Фэнтези'),
+                    ('Комиксный', 'Комиксный'),
+                    ('Кубизм', 'Кубизм'),
+                ),
         widget=forms.Select(attrs={'id': 'style'})
     )
 
     frm = forms.ChoiceField(
         label='Выберите соотношение сторон*',
         required=True,
-        choices=(('16:9', '16:9'), 
-                 ('1:1', '1:1')),
+        choices=(
+                ('1:1', '1:1'),
+                ('16:9', '16:9'),
+                ('9:16', '9:16'),
+                ('3:4', '3:4'),
+                ('4:3', '4:3'),
+            ),
         widget=forms.Select(attrs={'id': 'frm'})
     )
 
     quality = forms.ChoiceField(
-        label='Выберите качество*',
+        label='Выберите разрешение*',
         required=True,
-        choices=(('Низкое', 'Низкое'), 
-                 ('Среднее', 'Среднее'),
-                 ('Высокое', 'Высокое')),
+        choices=(
+                    ('Низкое', 'Низкое'), 
+                    ('Среднее', 'Среднее'),
+                    ('Высокое', 'Высокое'),
+                    ('Ультра', 'Ультра')
+                ),
         widget=forms.Select(attrs={'id': 'quality'})
     )
 
@@ -39,19 +53,19 @@ class CharacterForm(forms.Form):
     )
 
     action = forms.CharField(
-        label='Что делает? Какие предметы использует?',
+        label='Что делает? Какие предметы использует?*',
         max_length=100,
         widget=forms.Textarea(attrs={"id": "action"})
     )
 
     place = forms.CharField(
-        label='Где находится? Какие детали?',
+        label='Где находится? Какие детали?*',
         max_length=100,
         widget=forms.Textarea(attrs={"id": "avoidance"})
     ) 
 
     avoidance = forms.CharField(
-        label='Чего постараться избежать?',
+        label='Чего постараться избежать?*',
         max_length=100,
         widget=forms.Textarea(attrs={"id": "avoidance"})
     ) 
